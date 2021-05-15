0554094eb5cc99e8a5f44d2c52aa2f95838eb2e8
Paturages <a@faucheisti.eu>
2021-05-15 10:43:55 +0200

    Attempt to fix match feed

--- src/components/atoms/MatchFeed.svelte
+++ src/components/atoms/MatchFeed.svelte
@@ -4,9 +4,11 @@
   export let stage: Stage;
 
   const boldIf = (str: string | number, condition: boolean) => condition ? `**${str}**` : str;
+  const winCondition = 1 + (stage.best_of / 2 >> 0);
+</script>
 
-  $: value = `${stage.name}: M${match.id}
-${boldIf(`${match.players[0].player.username} ${match.points1}`, match.points1 == 4)} | ${boldIf(`${match.points2} ${match.players[1].player.username}`, match.points2 == 4)}
+<textarea readonly value={`${stage.name}: M${match.id}
+${boldIf(`${match.players[0].player.username} ${match.points1}`, match.points1 == winCondition)} | ${boldIf(`${match.points2} ${match.players[1].player.username}`, match.points2 == winCondition)}
 Rolls: ${match.rolls.map((roll, i) => boldIf(roll.value, roll.value > match.rolls[(i+1)%2]?.value)).join(' | ')}
 
 Protects:
@@ -15,10 +17,7 @@ ${match.protects.map(protect => `${protect.player.username}: [${protect.map.cate
 Bans:
 ${match.bans.map(ban => `${ban.player.username}: [${ban.map.category}] ${ban.map.name}`).join('\n')}
 
-MP Link: ${match.link}`;
-</script>
-
-<textarea readonly {value}></textarea>
+MP Link: ${match.link}`}></textarea>
 
 <style>
   textarea {
